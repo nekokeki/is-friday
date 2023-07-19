7bf9776244dfd7a28f07921879f6deeaab909987
Zyta <timer217@gmail.com>
2023-07-19 14:37:52 +0700

    fix layout bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,3 @@
-<script>
-	import '../app.css';
-
-	export const prerender = true;
-</script>
-
 <div class="min-h-screen flex flex-col bg-neutral-100">
 	<!-- <div>header</div> -->
 	<div class="flex-grow flex flex-col">
