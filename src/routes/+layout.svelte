fd8666188bd89935913a52da747603cea70b809a
Zyta <timer217@gmail.com>
2023-07-19 12:00:51 +0700

    add tailwindcss, add default layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="min-h-screen flex flex-col bg-neutral-100">
+	<!-- <div>header</div> -->
+	<div class="flex-grow flex flex-col">
+		<slot />
+	</div>
+	<!-- <div>footer</div> -->
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex-grow flex flex-col items-center justify-center">
+	<div class="container mx-auto text-center h-full">
+		<h1 class="text-7xl">Today is Friday</h1>
+	</div>
+</div>
