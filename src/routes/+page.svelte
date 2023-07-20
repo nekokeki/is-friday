fa7c9cf886f9b997803ed3894efb4962af88693d
Anya Quote <anya.quote@gmail.com>
2023-07-20 13:51:20 +0700

    fix: wrong is friday logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,6 @@
 
 <div class="flex-grow flex flex-col items-center justify-center">
 	<div class="container mx-auto text-center h-full">
-		<h1 class="text-7xl">Today {today.getDay() === 6 ? 'is' : 'is not'} Friday</h1>
+		<h1 class="text-7xl">Today {today.getDay() === 5 ? 'is' : 'is not'} Friday</h1>
 	</div>
 </div>
