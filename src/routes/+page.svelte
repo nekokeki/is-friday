5e6629c1ae94c49e9226f4c81eb415611210f92d
Anya Quote <anya.quote@gmail.com>
2023-07-20 13:53:27 +0700

    feat: show current day of week if not friday

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
-	const today = new Date();
+	const day = new Date().getDay();
+	const dayArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 </script>
 
 <div class="flex-grow flex flex-col items-center justify-center">
 	<div class="container mx-auto text-center h-full">
-		<h1 class="text-7xl">Today {today.getDay() === 5 ? 'is' : 'is not'} Friday</h1>
+		<h1 class="text-7xl">Today {day === 5 ? 'is' : 'is not'} Friday</h1>
+		{#if day !== 5}
+			<h2>Today is {dayArr[day]}</h2>
+		{/if}
 	</div>
 </div>
