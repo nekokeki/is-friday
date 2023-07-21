7f14ccfd1384adc84d4549dc80c42a736cc5a5d6
Anya Quote <120323703+AnyaQuote@users.noreply.github.com>
2023-07-21 14:23:00 +0700

    Typewriting effect on main page (#5)
    * build(+layout.ts): remove prerender
    
    * chore: ignore .vercel build folder
    
    * feat: create typewriter animation
    
    * chore: split tyewriter transition to another file
    
    * fix: import typewriter not found

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,27 @@
 <script>
-	const day = new Date().getDay();
+	import { typewriter } from '$lib/typewriter';
+	import { onMount } from 'svelte';
+
+	let day;
 	const dayArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const wait = () => new Promise((res) => setTimeout(res, 1000));
+
+	onMount(() => {
+		day = new Date().getDay();
+	});
 </script>
 
 <div class="flex-grow flex flex-col items-center justify-center">
 	<div class="container mx-auto text-center h-full">
-		<h1 class="text-7xl">Today {day === 5 ? 'is' : 'is not'} Friday</h1>
+		<h1 class="text-7xl">
+			Today
+			{#await wait()}
+				<span transition:typewriter>__</span>
+			{:then}
+				<span transition:typewriter>{day === 5 ? 'is' : 'is not'}</span>
+			{/await}
+			Friday
+		</h1>
 		{#if day !== 5}
 			<h2>Today is {dayArr[day]}</h2>
 		{/if}
